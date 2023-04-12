8c288393c309d7878284a7e0d145330cc168f276
vongo <vongo@andres-debian.home>
2023-04-12 16:06:51 +0200

    add lualine & nvim ts rainbow2 from community

--- plugins/community.lua
+++ plugins/community.lua
@@ -9,4 +9,6 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
 }
