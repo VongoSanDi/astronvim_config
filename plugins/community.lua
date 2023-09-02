617744f890d4481301b230c67d390ad2738128e3
vongo <vongo@andres-debian.home>
2023-09-02 21:57:59 +0200

    add markdown preview & delete rest file, use astrocommunity instead

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,6 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.json" },
@@ -12,7 +11,8 @@ return {
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.vue" },
-  -- { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  --  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.programming-language-support.rest-nvim" },
   { import = "astrocommunity.editing-support.nvim-devdocs" },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
 }
--- plugins/rest.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "rest-nvim/rest.nvim",
-  requires = { "nvim-lua/plenary.nvim" },
-}
